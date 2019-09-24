7a2e33504f680aab96fe48690b3bda57d20f108d
Abhishek Rawat <abhishekrawat5743@gmail.com>
2019-09-24 20:04:32 +0530

    Add files via upload

---
+++ header.php
@@ -0,0 +1,77 @@
+ <!doctype html>
+<html class="no-js" lang="en">
+
+<head>
+    <title>Softles | A Website Dvelopment Firm</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1" />
+    <!-- title -->
+    <meta name="description" content="lgdescription" />
+    <meta name="keywords" content="lgkeywords" />
+    <meta name="author" content="lgauthor">
+    <!-- favicon -->
+    <link rel="shortcut icon" href="images/icon/">
+    <!-- animation -->
+    <link rel="stylesheet" href="css/animate-purified.css" />
+    <!-- bootstrap -->
+    <!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"> -->
+    <link rel="stylesheet" href="css/bootstrap-purified.min.css" />
+    <!-- owl carousel -->
+    <link rel="stylesheet" href="css/owl-purified.transitions.css" />
+    <link rel="stylesheet" href="css/owl-purified.carousel.css" />
+    <!-- magnific popup -->
+    <link rel="stylesheet" href="css/magnific-popup-purified.css" />
+    <!-- base -->
+    <link rel="stylesheet" href="css/base-purified.css" />
+    <!-- elements -->
+    <link rel="stylesheet" href="css/elements-purified.css" />
+    <!-- responsive -->
+    <link rel="stylesheet" href="css/responsive-purified.css" />
+    <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome-purified.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Raleway&display=swap" rel="stylesheet">
+    <!--[if IE 9]>
+        <link rel="stylesheet" type="text/css" href="css/ie.css" />
+        <![endif]-->
+    <!--[if IE]>
+            <script src="js/html5shiv.min.js"></script>
+        <![endif]-->
+</head>
+<body>
+    <div id="page" class="page">
+        <header class="header-style4" id="header-section10">
+            <!-- nav -->
+            <nav class="navbar alt-font no-margin no-padding">
+                <div class="header-bottom">
+                    <div class="container navigation-menu">
+                        <div class="row">
+                            <div class="col-md-3 col-sm-4 col-xs-9">
+                                <a href="index.php">
+                                    <div class="brand">SOFTLES<br>
+                                        <div class="brandtag">"Less soft , more real"</div>
+                                    </div>
+                                </a>
+                            </div>
+                            <div class="col-md-9 col-sm-8 col-xs-3 position-inherit">
+                                <button data-target="#bs-example-navbar-collapse-1" data-toggle="collapse" class="navbar-toggle collapsed" type="button">
+                                    <span class="sr-only">Toggle navigation</span>
+                                    <span class="icon-bar"></span>
+                                    <span class="icon-bar"></span>
+                                    <span class="icon-bar"></span>
+                                </button>
+                                <div id="bs-example-navbar-collapse-1" class="collapse navbar-collapse pull-right">
+                                    <ul class="nav navbar-nav">
+                                        <li class="propClone"><a class="inner-link" href="index.php">Home</a></li>
+                                        <li class="propClone"><a class="inner-link" href="About Us.php">About Us</a></li>
+                                        <li class="propClone"><a class="inner-link" href="Services.php">Our Services</a></li>
+                                        <li class="propClone"><a class="inner-link" href="OurWork.php">Our Work</a></li>
+                                        <li class="propClone"><a class="inner-link" href="Contact Us.php">Contact Us</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+            <!-- end nav -->
+        </header>
